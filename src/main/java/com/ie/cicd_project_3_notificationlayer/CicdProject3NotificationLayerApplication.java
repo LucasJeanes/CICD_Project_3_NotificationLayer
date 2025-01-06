ddd8deaf6fc2d6ae054219d7cf16938a3e83f13b
Lucas Jeanes <lucasjeanes@gmail.com>
2025-01-06 17:00:26 +0000

    Project Setup

---
+++ src/main/java/com/ie/cicd_project_3_notificationlayer/CicdProject3NotificationLayerApplication.java
@@ -0,0 +1,13 @@
+package com.ie.cicd_project_3_notificationlayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdProject3NotificationLayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdProject3NotificationLayerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ie/cicd_project_3_notificationlayer/CicdProject3NotificationLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ie.cicd_project_3_notificationlayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdProject3NotificationLayerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
